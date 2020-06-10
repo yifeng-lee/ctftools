fc35646f5a9e1d2f296c7d512bfb46d4c13faef5
yifeng-lee <feng@m.scnu.edu.cn>
2020-06-10 19:07:36 +0800

    update

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 import os
 import sys
 
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
 WIN = sys.platform.startswith('win')
-if WIN:  # 如果是 Windows 系统，使用三个斜线
+if WIN:
     prefix = 'sqlite:///'
-else:  # 否则使用四个斜线
+else:
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-app.config['SECRET_KEY'] = os.urandom(10) 
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = os.urandom(10)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(
+    app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -29,8 +31,5 @@ def load_user(user_id):
 
 login_manager.login_view = 'login'
 
-import views,commands
+import views, commands
 import click
-
-
-
